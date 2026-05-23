1e8a74656294f820673ab5df6f00e88ae5117f40
Sneha Gade <sgade5591@gmail.com>
2026-05-23 18:50:22 +0530

    remove node_modules and .env from git tracking

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,14 +1,11 @@
-// api.js
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
-  baseURL: " https://classic-bakery-backend.onrender.com/api", // ✅ /api add kiya
-  withCredentials: true
+  baseURL: 'https://classic-bakery-backend-3.onrender.com/api'  // ✅ ye change karo
 });
 
-// 🔐 Token auto attach
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
